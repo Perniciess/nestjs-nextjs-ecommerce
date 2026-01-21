f33fd8711dbfb6ac0245b187428bee18e406b3af
Pernicies <74770654+Perniciess@users.noreply.github.com>
2026-01-21 22:02:03 +0300

    Update cookie.service.ts

--- backend/src/modules/auth/cookie.service.ts
+++ backend/src/modules/auth/cookie.service.ts
@@ -11,22 +11,23 @@ export class CookieService {
 		expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
 
 		res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
-			httpOnly: false,
+			httpOnly: true,
 			domain: process.env.COOKIE_DOMAIN,
 			expires: expiresIn,
-			secure: true,
-			sameSite: 'none', // 'lax' for subdomens
+			secure: false,
+			sameSite: 'lax', // 'lax' for subdomens
 		})
 	}
 
 	removeRefreshToken(res: Response) {
 		res.cookie(this.REFRESH_TOKEN_NAME, '', {
-			httpOnly: false,
+			httpOnly: true,
 			domain: process.env.COOKIE_DOMAIN,
 			expires: new Date(0),
-			secure: true,
-			sameSite: 'none', // 'lax' for subdomens
+			secure: false,
+			sameSite: 'lax', // 'lax' for subdomens
 		})
 	}
 }
+
 
